7506c64a0f02b3896b4d5afef4713f827a17f305
D2 <dsmontecastro1@up.edu.ph>
2023-10-07 18:31:01 +0800

    [GH-PAGE] Attempting fixes

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Sungka-Online/',
@@ -20,11 +21,19 @@ export default defineConfig({
   },
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '/@shared/utils',
-        replacement: path.resolve(__dirname, 'node_modules', 'shared', 'utils')
-      }
-    ]
+    alias: {
+      'react': path.resolve(__dirname, 'node_modules', 'react'),
+      '@shared/utils': path.resolve(__dirname, 'node_modules', '@shared', 'utils')
+    }
+    // alias: [
+    //   {
+    //     find: 'react',
+    //     replacement: path.resolve(__dirname, 'node_modules/react')
+    //   },
+    //   {
+    //     find: '/@shared/utils',
+    //     replacement: path.resolve(__dirname, 'node_modules', 'shared', 'utils')
+    //   }
+    // ]
   }
 })
