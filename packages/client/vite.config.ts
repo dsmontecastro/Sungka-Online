6b833ee0e7eed16d73cb59c97e3cfc96c4bd684d
D2 <dsmontecastro1@up.edu.ph>
2023-10-09 15:35:17 +0800

    [CLIENT] Minor changes

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const base = '/Sungka-Online/';
+const title = 'Sungka Online';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Sungka-Online/',
+  base: base,
   define: {
-    TITLE: JSON.stringify(process.env.npm_package_displayName)
+    TITLE: JSON.stringify(process.env.npm_package_displayName) || title
   },
   plugins: [react()],
 })
