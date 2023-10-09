93b3df9de76ff36bbba377e663580c2a2ebf4eab
D2 <dsmontecastro1@up.edu.ph>
2023-10-09 14:33:30 +0800

    [TS] Centralized TSConfigs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        project: './tsconfig.json',
+        project: '**/tsconfig.json',
         tsconfigRootDir: __dirname,
     },
 };
