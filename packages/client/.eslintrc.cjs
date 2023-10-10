50d987a27fc335be9f0496bfa86077c66f14f45b
D2 <dsmontecastro1@up.edu.ph>
2023-10-10 18:07:38 +0800

    [SERVER] Attempting deploy

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -8,6 +8,11 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
